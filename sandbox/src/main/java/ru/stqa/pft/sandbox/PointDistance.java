9e8a4911deef81ceef2b15a1f32e3696b596fe0c
SvetlanaArkhincheeva <arhincheeva@gmail.com>
2017-05-14 22:06:15 +0700

    Задание №3
    Созданы несколько тестов для класса Point, которые проверяют, что
    расстояние между точками вычисляется правильно.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/PointDistance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointDistance.java
@@ -11,6 +11,8 @@ public class PointDistance {
         Point p2 = new Point(13, 18);
         Point p3 = new Point(2.5, 4.5);
         Point p4 = new Point(7, 8);
+        Point p5 = new Point(0.5,0.7);
+        Point p6 = new Point(0,0);
         /*p1.x=0;
         p2.x=10;
         p1.y=0;
@@ -19,7 +21,7 @@ public class PointDistance {
         System.out.println("Расстояние между точками p1 и p2 = " + p1.distance(p1, p2));
         System.out.println("Расстояние между точками p3 и p4 = " + p3.distance(p3, p4));
         System.out.println("Расстояние между точками p2 и p4 = " + p2.distance(p2, p4));
-
+        System.out.println("Расстояние между точками p5 и p6 = " + p6.distance(p5, p6));
         /*public static double distance(Point p1, Point p2) {
             return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y));
         }*/
---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -0,0 +1,43 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+/**
+ * Created by Светлана on 14.05.2017.
+ */
+public class PointTest {
+    @Test
+    public void testDistance1() {
+        Point p1 = new Point(0,0);
+        Point p2 = new Point(0,0);
+        Assert.assertEquals(p1.distance(p1,p2), 0.0);
+    }
+
+    @Test
+    public void testDistance2() {
+        Point p1 = new Point(0,0);
+        Point p2 = new Point(10,10);
+        Assert.assertEquals(String.format("%.2f",p1.distance(p1,p2)), "14,14");
+    }
+
+    @Test
+    public void testDistance3() {
+        Point p1 = new Point(0.5,0.7);
+        Point p2 = new Point(0,0);
+        Assert.assertEquals(p1.distance(p1,p2), 0.8602325267042626);
+    }
+
+    @Test
+    public void testDistance4() {
+        Point p1 = new Point(-5,-10);
+        Point p2 = new Point(-99.15,-99.6);
+        Assert.assertEquals(String.format("%.2f",p1.distance(p1,p2)), "129,97");
+    }
+
+    @Test
+    public void testDistance5() {
+        Point p1 = new Point(-5,-10);
+        Point p2 = new Point(99.15,99.6);
+        Assert.assertEquals(String.format("%.2f",p1.distance(p1,p2)), "151,19");
+    }
+}
