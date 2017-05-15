6fda72c0a7c5f9c5faa415518dc3a084c60b3fff
SvetlanaArkhincheeva <arhincheeva@gmail.com>
2017-05-15 17:20:49 +0700

    Задание 2_Исправления

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -14,7 +14,8 @@ public class Point {
 
     }
 
-    public double distance(Point p1, Point p2) {
-        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    double distance(Point a) {
+        return Math.sqrt( Math.pow(x-a.x, 2) + Math.pow(y-a.y, 2) );
+
     }
 }
