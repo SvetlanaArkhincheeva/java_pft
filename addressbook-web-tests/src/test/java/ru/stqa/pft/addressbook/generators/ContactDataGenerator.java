e34f71aa64d616d7a1990116ad4ddc63fb8731b1
SvetlanaArkhincheeva <arhincheeva@gmail.com>
2017-06-19 07:59:32 +0700

    Лекция_6.4

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -0,0 +1,57 @@
+package ru.stqa.pft.addressbook.generators;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import ru.stqa.pft.addressbook.model.ContactData;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactDataGenerator {
+
+    @Parameter(names = "-c", description = "Contact count")
+    public int count;
+
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
+
+
+    public static void main(String[] args) throws IOException {
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+        generator.run();
+    }
+
+    private void run() throws IOException {
+        List<ContactData> contacts = generateContacts(count);
+        save(contacts, new File(file));
+    }
+
+    private void save(List<ContactData> contacts, File file) throws IOException {
+        System.out.println(new File (".").getAbsolutePath());
+        Writer writer = new FileWriter(file);
+        for (ContactData contact : contacts) {
+            writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getLastname(), contact.getAddress(), contact.getHomephone(), contact.getWorkphone(), contact.getMobilephone(), contact.getEmail(), contact.getEmail2(), contact.getEmail3()));;
+        }
+        writer.close();
+    }
+
+    private List<ContactData> generateContacts(int count) {
+        List<ContactData> contacts = new ArrayList<ContactData>();
+        for (int i = 0; i < count; i++) {
+            contacts.add(new ContactData().withFirstname(String.format("Sveta %s", i)).withLastname(String.format("Arkhincheeva %s", i)).withAddress(String.format("Novosibirsk %s", i)).withHomephone(String.format("33366699 %s", i)).withMobilephone(String.format("89139999999 %s", i)).withWorkphone(String.format("54654 %s", i)).withEmail(String.format("dfdgfg@fdfg.ru %s", i)).withEmail2(String.format("dfdgfg@fdfg.ru %s", i)).withEmail3(String.format("dfdgfg@fdfg.ru %s", i)));
+        }
+        return contacts;
+    }
+}
+
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,23 +1,37 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.By;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
-
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
+    @DataProvider
+    public Iterator<Object[]> validContacts() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[] {"firstname1", "lastname1", "address1", "home1", "mobile1", "work1", "email 11", "email2", "email3"});
+        //list.add(new Object[] {"firstname2", "lastname2","test2", "phone 2", "email 2", "group2", "address2", "mobile2"});
+        //list.add(new Object[] {"firstname3", "lastname3","test3", "phone 3", "email 3", "group3", "address3", "mobile3"});
+        return list.iterator();
+    }
 
-    @Test
-    public void testContactCreation() {
+
+    @Test(dataProvider = "validContacts")
+    public void testContactCreation(String firstname, String lastname, String address, String home, String mobile, String work, String email, String email2, String email3) {
+        //File photo = new File("src/test/resources/stru.png");
+        ContactData contact = new ContactData().withFirstname(firstname).withLastname(lastname).withAddress(address).withHomephone(home).withMobilephone(mobile).withWorkphone(work).withtEmail(email).withEmail2(email2).withEmail3(email3);
         app.goTo().homePage();
         Contacts before = app.contact().all();
-        File photo = new File("src/test/resources/stru.png");
-        ContactData contact = new ContactData()
-                .withFirstname("Svetlana").withLastname("Arkhincheeva").withPhonenumber("123").withtEmail("test@test.ru").withGroup("test1").withAddress("Test").withMobilephone("+79132222222").withPhoto(photo);
+
+
         app.contact().create(contact, true);
         app.goTo().homePage();
         assertThat(app.contact().count(), equalTo(before.size() + 1));
