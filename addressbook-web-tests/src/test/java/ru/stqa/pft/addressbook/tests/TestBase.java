deff57f126caf095f1450862b468057bb44ac4e2
SvetlanaArkhincheeva <arhincheeva@gmail.com>
2017-06-20 13:34:00 +0700

    Задание №14

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -6,7 +6,8 @@ import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
-    protected final static ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    protected final static ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
     @BeforeSuite
     public void setUp() throws Exception {
