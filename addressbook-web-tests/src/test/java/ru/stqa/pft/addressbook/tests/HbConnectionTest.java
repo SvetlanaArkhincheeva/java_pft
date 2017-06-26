6ade8653657f5bac6730008086add9a4ff6f75e8
SvetlanaArkhincheeva <arhincheeva@gmail.com>
2017-06-26 16:53:00 +0700

    Лекция_7.2

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTest.java
@@ -6,30 +6,25 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
-
 import java.util.List;
 
 public class HbConnectionTest {
     private SessionFactory sessionFactory;
 
-    @BeforeClass
+   @BeforeClass
 
     protected void setUp() throws Exception {
-        // A SessionFactory is set up once for an application!
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure() // configures settings from hibernate.cfg.xml
+                .configure()
                 .build();
         try {
             sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
         }
         catch (Exception e) {
             e.printStackTrace();
-            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
-            // so destroy it manually.
             StandardServiceRegistryBuilder.destroy( registry );
         }
     }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -8,33 +8,30 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
-
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
 public class TestBase {
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
     Logger logger = LoggerFactory.getLogger(TestBase.class);
-    protected final static ApplicationManager app
-            = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
     @BeforeSuite
     public void setUp() throws Exception {
         app.init();
     }
 
-    @AfterSuite (alwaysRun = true)
+    @AfterSuite(alwaysRun = true)
     public void tearDown() {
         app.stop();
     }
 
     @BeforeMethod
     public void logTestStart(Method m, Object[] p) {
-        logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+        logger.info("Start test" + m.getName() +  " with parameters " + Arrays.asList(p));
     }
 
-    @AfterMethod (alwaysRun = true)
+    @AfterMethod(alwaysRun = true)
     public void logTestStop(Method m) {
-        logger.info("Stop test " + m.getName());
-
+        logger.info("Stop test" + m.getName());
     }
 }
