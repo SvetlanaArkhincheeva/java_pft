f94554b772489a54f52f2d10ca45285162f61a6e
SvetlanaArkhincheeva <arhincheeva@gmail.com>
2017-07-04 15:25:59 +0700

    Лекция_8.6

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -5,6 +5,7 @@ public class RegistrationTests extends TestBase{
 
     @Test
     public void testRegistration() {
+
         app.registration().start("user1", "user1@localhost.localdomain");
     }
 }
